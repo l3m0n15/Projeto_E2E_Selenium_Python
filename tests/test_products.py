d29d49533c53d7c6a681c010dbb86a6bdee4d7ba
l3m0n15 <sn.andreryan@live.com>
2026-04-11 22:40:26 -0300

    Teste novo

--- tests/test_products.py
+++ tests/test_products.py
@@ -12,7 +12,19 @@ import pytest
 ])
 
 def test_add_very_products(usuario_login, products):
-    add_very_products(usuario_login, products)
+    add_very_products(usuario_login, [products])
 
-    
+def test_add_very_products(checkout_cart):
+    produto = [
+"Sauce Labs Backpack", 
+"(Sauce Labs Bike Light",
+"Sauce Labs Bolt T-Shirt",
+"Sauce Labs Fleece Jacket",
+"Sauce Labs Onesie",
+"Test.allTheThings() T-Shirt (Red)"
 
+carrinho = add_very_products():
+assert carrinho == checkout_cart
+
+
+]
